a323ba0cc1007b21b76f35f8a6770b266233e7d5
tomoyosiki <tomoyosiki@gmail.com>
2019-11-21 17:46:47 -0500

    Update calling method

--- rs-imshow.py
+++ rs-imshow.py
@@ -17,9 +17,9 @@ print("Depth Scale is: " , depth_scale)
 
 align_to = rs.stream.color
 align = rs.align(align_to)
-cv2.namedWindow('win1', cv2.WINDOW_AUTOSIZE)
-cv2.namedWindow('win2', cv2.WINDOW_AUTOSIZE)
-cv2.namedWindow('win3', cv2.WINDOW_AUTOSIZE)
+#cv2.namedWindow('win1', cv2.WINDOW_AUTOSIZE)
+#cv2.namedWindow('win2', cv2.WINDOW_AUTOSIZE)
+#cv2.namedWindow('win3', cv2.WINDOW_AUTOSIZE)
 count = 0
 try:
     while True:
@@ -33,11 +33,11 @@ try:
 
         if not depth_frame or not color_frame:
             continue
-        print(count)
+        print('Frame ', count)
         count = count + 1
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
-        
+        #color_image = cv2.fastNlMeansDenoisingColored(color_image, None, 10, 10, 7, 15)
         #color_image = cv2.fastNlMeansDenoisingColored(color_image,None,10,10,7,21)
         gray_color_image = cv2.cvtColor(color_image, cv2.COLOR_RGB2GRAY)
         #gray_color_image = gray_color_image * 0.75
@@ -45,17 +45,31 @@ try:
         height, width = gray_color_image.shape
         #print('RGB', color_image)
         hls_image = cv2.cvtColor(color_image, cv2.COLOR_RGB2HLS)
+        hsv_image = cv2.cvtColor(color_image, cv2.COLOR_RGB2HSV)
         #print('HLS', hls_image)
-        lower = np.array([0, 200, 0])
-        upper = np.array([255, 255, 255])
+        lower = np.array([0, 175, 0])
+        upper = np.array([180, 200, 255])
         mask = cv2.inRange(hls_image, lower, upper)
-        
+        #mask = cv2.bitwise_not(mask)
 
         mask_gray_color_image = np.bitwise_and(gray_color_image2, mask)
         #mask = cv2.fastNlMeansDenoising(gray_color_image, None, 9, 13)
-        gaussBlur_mask = cv2.GaussianBlur(mask_gray_color_image, (7, 7), 0)
-        canny_mask = cv2.Canny(gaussBlur_mask, 100, 200, apertureSize = 5, L2gradient=True)
-        canny_mask = canny_mask[height // 2: height, 0:width]
+        #gaussBlur_mask = cv2.GaussianBlur(mask_gray_color_image, (7, 7), 0)
+        mask = cv2.fastNlMeansDenoising(mask, None, 9, 13)
+        edges = cv2.Canny(mask, 200, 400)
+        
+        mask = np.zeros_like(edges)
+        height, width = edges.shape
+        polygon = np.array([[
+            (0, height / 2),
+            (width, height / 2),
+            (width, height),
+            (0, height)
+        ]], np.int32)
+        cv2.fillPoly(mask, polygon, 255)
+        cropped_edges = cv2.bitwise_and(edges, mask)
+
+        #canny_mask = canny_mask[height // 2: height, 0:width]
 
         #mask = cv2.inRange(gray_color_image, 200, 255)
         #print(width, height, mask.shape)
@@ -63,10 +77,15 @@ try:
         #print(mask.shape)
         #gaussBlur_mask = cv2.GaussianBlur(mask, (7, 7), 0)
         #canny_mask = cv2.Canny(gaussBlur_mask, 100, 200, apertureSize = 5)
-        print(type(canny_mask))
-        lines =	cv2.HoughLinesP(canny_mask, 1, np.pi / 180, 50, 50, 10)
-        print(width, height, canny_mask.shape)
+        lines =	cv2.HoughLinesP(cropped_edges, 
+                                1, 
+                                np.pi / 180, 
+                                10, 
+                                np.array([]), 
+                                minLineLength=8, maxLineGap=4)
+        #print(width, height, canny_mask.shape)
         if type(lines) != type(None):
+            print("find ",len(lines), "lines")
             for line in lines:
                 print(line)
                 x1,y1,x2,y2 = line[0]
@@ -99,18 +118,20 @@ try:
                         y2 = int(weight * x2 + bias)
                         break
                 '''
-                cv2.line(color_image,(x1,y1 + height // 2),(x2,y2 + height // 2),(0,0,255),2)
+                cv2.line(color_image,(x1,y1),(x2,y2),(0,0,255),2)
         cv2.imshow('win1', color_image)
         #cv2.imshow('win2', gray_color_image)
         #cv2.imshow('win3', gray_color_image2)
         #cv2.imshow('win4', hls_image)
-        #cv2.imshow('win5', mask)
-        #cv2.imshow('win6', gaussBlur_mask)
-        cv2.imshow('win7', canny_mask)
+        #cv2.imshow('win5', hsv_image)
+        cv2.imshow('win6', mask_gray_color_image)
+        #cv2.imshow('win7', gaussBlur_mask)
+        cv2.imshow('win8', cropped_edges)
         key = cv2.waitKey(1)
         # Press esc or 'q' to close the image window
         if key & 0xFF == ord('q') or key == 27:
             cv2.destroyAllWindows()
             break
+
 finally:
     pipe.stop()
